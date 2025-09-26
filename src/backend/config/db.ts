411b777a1a179097ed945fac222289547caa26bd
gerry <krittameth2020@gmail.com>
2025-09-26 12:33:55 +0700

    functional api

--- src/backend/config/db.ts
+++ src/backend/config/db.ts
@@ -1,4 +1,8 @@
 import mysql from 'mysql2';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
@@ -6,4 +10,31 @@ const pool = mysql.createPool({
     database: process.env.DB_DATABASE
 });
 
-export default pool.promise();
+const promisePool = pool.promise();
+
+export const getCar = async() => {
+    const [rows] = await promisePool.query("SELECT * FROM car");
+    return rows;
+}
+
+export const getMatches = async() => {
+    const [rows] = await promisePool.query("SELECT * FROM matches");
+    return rows
+}
+
+export const getMotorsport = async() => {
+    const [rows] = await promisePool.query("SELECT * FROM motorsport");
+    return rows
+}
+
+export const getTeam = async() => {
+    const [rows] = await promisePool.query("SELECT * FROM team");
+    return rows
+}
+
+export const getMatchParticipating = async() => {
+    const [rows] = await promisePool.query("SELECT * FROM match_participating");
+    return rows
+}
+
+export default promisePool;
--- src/backend/routes/index.ts
+++ src/backend/routes/index.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express'
+import { getCar, getMotorsport, getMatches, getTeam, getMatchParticipating } from '../config/db'
 
 const router = Router();
 
@@ -7,6 +8,46 @@ router.get('/', (req, res) => {
         message: "Hello, world."
     })
 });
+
+router.get('/car', async (req, res) => {
+    const data = await getCar();
+    console.log("GET CAR");
+    res.send({
+        data
+    });
+});
+
+router.get('/matches', async (req, res) => {
+    const data = await getMatches();
+    console.log("GET MATCHES");
+    res.send({
+        data
+    });
+});
+
+router.get('/team', async (req, res) => {
+    const data = await getTeam();
+    console.log("GET TEAM");
+    res.send({
+        data
+    });
+});
+
+router.get('/motorsport', async (req, res) => {
+    const data = await getMotorsport();
+    console.log("GET MOTORSPORT");
+    res.send({
+        data
+    });
+});
+router.get('/participation', async (req, res) => {
+    const data = await getMatchParticipating();
+    console.log("GET MATCH_PARTICIPATION");
+    res.send({
+        data
+    });
+});
+
 
 
 export default router;
